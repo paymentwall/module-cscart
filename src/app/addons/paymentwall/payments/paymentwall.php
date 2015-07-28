7dc4e037345bee36ec544777b23054966f830efa
Dzung Tran <tiendung@paymentwall.com>
2015-07-28 15:59:16 +0700

    Added User Profile API support

--- src/app/addons/paymentwall/payments/paymentwall.php
+++ src/app/addons/paymentwall/payments/paymentwall.php
@@ -5,7 +5,7 @@ if (!defined('AREA')) { die('Access denied'); }
 fn_change_order_status($order_info['order_id'], 'O');
 
 // Redirect to widget page
-$postUrl = fn_url('paymentwall.payment&order_id=' . $order_info['order_id']);
+$postUrl = fn_url('paymentwall.payment');
 $_SESSION['pw_order_id'] = $order_info['order_id'];
 header("Location: {$postUrl}");
 
